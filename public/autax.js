3be5deb278c05f62d7fe8861405d9ab3a20761ee
Massimo Berta <massimo.berta@gmail.com>
2016-05-08 12:12:13 +0100

    update js
    final version of js frontend

--- public/autax.js
+++ public/autax.js
@@ -8,13 +8,28 @@ $("document").ready(function(){
     });
 
 $("#getPayslip").click(function(){
-    console.log("Getting payslip")
-    $.post( "/payee", { firstName: $("#firstName").val(), lastName: $("#lastName").val(), period: $("#period").val(), amount: $("#amount").val(), pension: $("#pension").val() })
+    $.post( "/payee", { firstName: $("#firstName").val(), lastName: $("#lastName").val(), period: $("#period").val(), amount: $("#amount").val(), pension: $("#pension").val(), medicare: $("#medicare").val() })
     .done(function( data ) {
         if(data.hasOwnProperty("error")){
-            console.log(data)
             $("#error_text").text(data.error);
             $("#error").addClass('in');
+        } else {
+            $("#payslipDocTitle").html(data.firstName+" "+data.lastName+"<br>"+data.period)
+            $("#amountPrint").text(data.amount+"$")
+            $("#taxBandPrint").text(data.taxBand)
+            $("#grossAmountPrint").text(data.grossAmount+"$")
+            $("#incomeTaxPrint").text(data.incomeTax+"$")
+            $("#medicarePrint").text("Yes")
+            if(data.medicare){
+                $("#medicarePrint").text("No")
+                $("#medicareInfo").css("display", "inline")
+                $("#medicareAmountPrint").text(data.medicareAmount+"$")
+                $("#medicareCreditPrint").text(data.medicareCredit+"$")
+            }
+            $("#pensionPrint").text(data.pension)
+            $("#pensionAmountPrint").text(data.pensionAmount+"$")
+            $("#netAmountPrint").text(data.netAmount+"$")
+            $('#payslip').modal("toggle")
         }
     });
 })
